6c1bb26bbce07b42ad179557501d6b96c43e7ff0
SRIDHAR-08 <sridhar200288@gmail.com>
2024-01-03 13:15:31 +0530

    vn

--- sridhar/src/App.js
+++ sridhar/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reloadjkb.
+          Edit <code>src/App.js</code> and save to reloadjkbjhgvc.
         </p>
         <a
           className="App-link"
